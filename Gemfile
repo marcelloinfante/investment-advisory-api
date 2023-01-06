7822a437489e7c94f64aebeb22c59a7d4c63619f
Marcello Infante <contato@marcelloinfante.com.br>
2023-01-06 10:34:49 -0300

    feat: improve

--- Gemfile
+++ Gemfile
@@ -51,14 +51,15 @@ gem "discard", "~> 1.2"
 # Use interactor to centralize business rules
 gem "interactor", "~> 3.0"
 
+gem "factory_bot_rails"
+gem "faker"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
   gem "shoulda-matchers", "~> 5.0"
   gem "simplecov", require: false
   gem "rspec-rails", "~> 6.0.0"
-  gem "factory_bot_rails"
-  gem "faker"
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-user = FactoryBot.create(:user, email: "test@test.com", first_name: "John", last_name: "Marshal", password: "test123", password_confirmation: "test123")
+user = FactoryBot.create(:user, email: "marcelloinfante@gmail.com", first_name: "Marcello", last_name: "Infante", password: "test123", password_confirmation: "test123")
 
 clients = FactoryBot.create_list(:client, 5, user:)
 
@@ -6,7 +6,16 @@ clients.each do |client|
   assets = FactoryBot.create_list(:asset, 5, client:)
   
   assets.each do |asset|
-    FactoryBot.create_list(:simulation, 5, asset:)
+    params = FactoryBot.attributes_for(:simulation)
+    params[:asset] = asset
+    params[:quotation_date] = params[:quotation_date].to_s
+    params[:new_asset_expiration_date] = params[:new_asset_expiration_date].to_s
+
+    interector = Simulation::BuildAttributes.call(params:)
+
+    if interector.success?
+      Simulation.create!(interector.result)
+    end
   end
 end
 
